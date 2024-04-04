349d16b48e6c6a723e07788ef990068121a9c84a
Luc191 <lucifer-191@mail.ru>
2024-04-04 10:12:17 +0300

    Лаба 2

---
+++ Lab_2_1/Lab_2_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Server server = new Server();
+server.Start();
+await server.Receive();
+class Server {
+    IPEndPoint ep = new IPEndPoint(IPAddress.Any, 8888);
+    Socket tcpListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    Dictionary <string, string> words = new Dictionary<string, string>()
+    {
+       { "привет", "и тебе привет" },
+       { "как дела?", "нормально" },
+       { "пока", "и тебе пока" },
+    };
+    public void Start()
+    {
+        tcpListener.Bind(ep);
+        tcpListener.Listen();    // запускаем сервер
+        Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+    }
+    public async Task Receive()
+    {
+        try
+        {
+
+            while (true)
+            {
+                // получаем подключение в виде TcpClient
+                using var tcpClient = await tcpListener.AcceptAsync();
+
+                // буфер для накопления входящих данных
+                var response = new List<byte>();
+                // буфер для считывания одного байта
+                var bytesRead = new byte[1];
+                while (true)
+                {
+                    // считываем данные до конечного символа
+                    while (true)
+                    {
+                        var count = tcpClient.Receive(bytesRead);
+                        // смотрим, если считанный байт представляет конечный символ, выходим
+                        if (count == 0 || bytesRead[0] == '\n') break;
+                        // иначе добавляем в буфер
+                        response.Add(bytesRead[0]);
+                    }
+                    var word = Encoding.UTF8.GetString(response.ToArray());
+                    // если прислан маркер окончания взаимодействия,
+                    // выходим из цикла и завершаем взаимодействие с клиентом
+                    if (word == "END") break;
+
+                    Console.WriteLine($"Вам отправили: {word}");
+                    // находим слово в словаре и отправляем обратно клиенту
+                    if (!words.TryGetValue(word, out var translation)) translation = "Не знаю как ответить :(";
+                    // добавляем символ окончания сообщения 
+                    translation += '\n';
+                    // отправляем перевод слова из словаря
+                    await tcpClient.SendAsync(Encoding.UTF8.GetBytes(translation));
+                    response.Clear();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+}
---
+++ Lab_2_4/Lab_2_4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+// слова для отправки для получения перевода
+
+var sw = new Stopwatch();
+sw.Start();
+Client client = new Client();
+client.Connect();
+client.Send();
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+class Client
+{
+    string[] words = new string[] { "привет", "как дела?", "что делаешь?" };
+    Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    public void Connect()
+    {
+        try
+        {
+            tcpClient.Connect("127.0.0.1", 8888);
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Не удалось подключиться");
+        }
+    }
+    public void Send()
+    {
+        // буфер для входящих данных
+        var response = new List<byte>();
+        foreach (var word in words)
+        {
+            // считыванием строку в массив байт
+            // при отправке добавляем маркер завершения сообщения - \n
+            byte[] data = Encoding.UTF8.GetBytes(word + '\n');
+            // отправляем данные
+            tcpClient.Send(data);
+
+            // буфер для считывания одного байта
+            var bytesRead = new byte[1];
+            // считываем данные до конечного символа
+            while (true)
+            {
+                var count = tcpClient.Receive(bytesRead);
+                // смотрим, если считанный байт представляет конечный символ, выходим
+                if (count == 0 || bytesRead[0] == '\n') break;
+                // иначе добавляем в буфер
+                response.Add(bytesRead[0]);
+            }
+            var translation = Encoding.UTF8.GetString(response.ToArray());
+            Console.WriteLine($"Вы отправили: {word}. Вам ответили: {translation}");
+            response.Clear();
+        }
+
+        // отправляем маркер завершения подключения - END
+        tcpClient.Send(Encoding.UTF8.GetBytes("END\n"));
+        Console.WriteLine("Все сообщения отправлены");
+    }
+}
+
+
